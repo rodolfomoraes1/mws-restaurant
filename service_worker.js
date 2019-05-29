285898906999b1db5291eef9556e4ebbc1f6d967
Rodolfo Martins <rodolfomoraes1@gmail.com>
2019-05-29 20:45:12 -0300

    fixed service worker to receive full path of files and with root directory

--- service_worker.js
+++ service_worker.js
@@ -4,6 +4,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheVersion).then(function(cache) {
     return cache.addAll([
+      './',
       'index.html',
       'restaurant.html',
       '/css/styles.css',
@@ -12,7 +13,8 @@ self.addEventListener('install', function(event) {
       '/js/main.js',
       '/js/restaurant_info.js',
       'data/restaurants.json',
-      '/img/*',
+      '/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg', '/img/5.jpg',
+      '/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg', '/img/10.jpg',
       'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
       'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
     ]).catch();
